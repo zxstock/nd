a4b7969cc42db9bf807888465b638a66eec1183f
Zhen Xu <zxu@gmw.com>
2018-10-25 11:32:37 -0700

    Initial commit

---
+++ nd/items.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+#https://finviz.com/quote.ashx?t=aapl
+#https://www.ipoboutique.com/ipo-calendar.html
+import scrapy
+
+
+class NdItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
